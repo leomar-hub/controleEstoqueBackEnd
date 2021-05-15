726f3779bc6a47ecdda36fdd474ca2ebfc9d7cbd
Sergio Leomar <sergio.dias2@fatec.sp.gov.br>
2021-05-15 14:44:39 -0300

    Adicionando a classe usuario

--- controleEstoque/src/main/java/com/br/evesersoftware/controller/IndexUsuarioController.java
+++ controleEstoque/src/main/java/com/br/evesersoftware/controller/IndexUsuarioController.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +39,26 @@ public class IndexUsuarioController {
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/", produces="application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/", produces="application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete (@PathVariable (value = "id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "Ok";
 	}
 
 }
