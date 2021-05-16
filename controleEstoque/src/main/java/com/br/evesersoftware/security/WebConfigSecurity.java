25a4e32e07031b91c49bf7fefe3cb1c9b8973062
Sergio Leomar <sergio.dias2@fatec.sp.gov.br>
2021-05-16 01:39:41 -0300

    Confirmano: Classes de configuração para autenticação por token

--- controleEstoque/src/main/java/com/br/evesersoftware/security/WebConfigSecurity.java
+++ controleEstoque/src/main/java/com/br/evesersoftware/security/WebConfigSecurity.java
@@ -3,8 +3,12 @@ package com.br.evesersoftware.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.br.evesersoftware.service.ImplementacaoUserDetailsService;
 
@@ -16,8 +20,23 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 	
 	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+	}
+	
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(implementacaoUserDetailsService);
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 }
