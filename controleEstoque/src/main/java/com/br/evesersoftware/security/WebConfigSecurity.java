87acf1b79139de874a2da137ffc54f206291b6f8
Sergio Leomar <sergio.dias2@fatec.sp.gov.br>
2021-05-16 01:38:37 -0300

    Classes de configuração para autenticação por token

---
+++ controleEstoque/src/main/java/com/br/evesersoftware/security/WebConfigSecurity.java
@@ -0,0 +1,23 @@
+package com.br.evesersoftware.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.br.evesersoftware.service.ImplementacaoUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(implementacaoUserDetailsService);
+	}
+	
+}
---
+++ controleEstoque/src/main/java/com/br/evesersoftware/service/ImplementacaoUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.br.evesersoftware.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.br.evesersoftware.model.Usuario;
+import com.br.evesersoftware.repository.UsuarioRepository;
+
+@Service
+public class ImplementacaoUserDetailsService  implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+		
+		if(usuario == null) {
+			throw new UsernameNotFoundException("Usuário não foi encontrado");
+		}
+		
+		return new User(
+				usuario.getLogin(), 
+				usuario.getSenha(), 
+				usuario.getAuthorities()
+				);
+	}
+
+}
